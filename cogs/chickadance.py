bece44489b4f215bbc190195944b53624afa092d
J_C___ <jcsumlin@gmail.com>
2019-02-08 20:37:08 -0600

    edited chikadance

--- cogs/chickadance.py
+++ cogs/chickadance.py
@@ -11,7 +11,7 @@ class Chikadance:
                       'https://cdn.discordapp.com/attachments/486899116299911178/543613202177720320/image0.gif',
                       'https://cdn.discordapp.com/attachments/517546603473797141/540644284106276864/k757zzgurid21.gif']
 
-    @commands.command(name='chika')
+    @commands.command(name='chikadance', aliases=['chika'])
     async def chikadance(self):
        choice = random.choice(self.dance)
        await self.bot.say(choice)
