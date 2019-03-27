469a9c33d461cc0f0effcbe9a743a03d95d60cdb
J_C___ <jcsumlin@gmail.com>
2019-03-27 16:15:44 -0500

    reddit mod cog

--- cogs/redditmod.py
+++ cogs/redditmod.py
@@ -13,8 +13,7 @@ class Reddit:
 
     @commands.group(aliases=["sr"], pass_context=True, no_pm=True)
     async def subreport(self, ctx, link):
-        if ctx.message.channel == '486883481843138560':
-            if 'reddit.com' in ctx.message.content:
+        if ctx.message.channel.id == '486883481843138560':
                 embed = discord.Embed(title="Should this post be removed?",
                                       url=link)
                 embed.set_thumbnail(
@@ -22,6 +21,7 @@ class Reddit:
                 message = await self.bot.send_message(ctx.message.channel, embed=embed)
                 await self.bot.add_reaction(message, u"\u2705")
                 await self.bot.add_reaction(message, u"\u274C")
+                await self.bot.delete_message(ctx.message)
 
 def setup(bot):
     bot.add_cog(Reddit(bot))
