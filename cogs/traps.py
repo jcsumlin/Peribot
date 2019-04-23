971958e97af078ee18840064237af4d823913a98
J_C___ <jcsumlin@gmail.com>
2019-04-23 15:00:00 -0500

    Added trap add functionality

--- cogs/traps.py
+++ cogs/traps.py
@@ -1,37 +1,55 @@
+import json
+import random
+import re
+
 import discord
 from discord.ext import commands
-import random
+
+from .utils.dataIO import dataIO
+from .utils.dataIO import fileIO
+
 
 class Trap:
 
     def __init__(self, bot):
         self.bot = bot
-        self.trap_gifs = ['https://media1.tenor.com/images/b43df533b92a60a6302a3fceb6ea532c/tenor.gif',
-                     'https://media1.tenor.com/images/1b011a65b404188031d13c866eb7c793/tenor.gif',
-                     'https://media1.tenor.com/images/4fd0125d511e202a50bd41e120187506/tenor.gif',
-                     'https://thumbs.gfycat.com/WhichGratefulBettong-small.gif',
-                     'https://steamuserimages-a.akamaihd.net/ugc/848214956775689562/2DE796CA8D75CEA67F717E22885461760D546479/',
-                     'https://i.pinimg.com/originals/36/e6/71/36e671921eecfefde01cbfdd35c25997.gif',
-                     'https://66.media.tumblr.com/6ebba4bdefbb672416e86145b621bef9/tumblr_ov0yh7aLHZ1td5qjgo1_400.gif',
-                     'https://pa1.narvii.com/6501/0d985d7b0fbdfed6eaac2a7d0c16616301c30de7_hq.gif',
-                     'https://66.media.tumblr.com/23ec2fb29499576f465a45557b2b3a55/tumblr_omk741Am1E1qa94xto1_400.gif',
-                     'https://66.media.tumblr.com/0ea8490d87e6a33684153587a70c8359/tumblr_nkcnd3OWw41rs4yfmo1_400.gif']
+        self.trap_gifs = fileIO("data/lewd/traps/traps.json", "load")
 
-    @commands.command(pass_context=True, no_pm=True)
-    async def trap(self):
+    @commands.group(pass_context=True)
+    async def trap(self, ctx):
         '''
         Returns either a gif of Star Wars Trap or Anime Trap... Are you a gambling man?
         :return: What kind of trap is it?
         '''
-        type = random.randint(1, 2)
-        if type == 1:
-            embed = discord.Embed(title="Now ***that's*** a trap!", color=0xffd2e8)
-            embed.set_image(url=random.choice(self.trap_gifs))
-            await self.bot.say(embed=embed)
-        elif type == 2:
-            embed = discord.Embed(title="ITS A TRAP!", color=0x381010)
-            embed.set_image(url='https://media.giphy.com/media/8McNH1aXZnVyE/giphy.gif')
-            await self.bot.say(embed=embed)
+        if ctx.invoked_subcommand is None:
+            type = random.randint(1, 2)
+            if type == 1:
+                embed = discord.Embed(title="Now ***that's*** a trap!", color=0xffd2e8)
+                embed.set_image(url=random.choice(self.trap_gifs))
+                await self.bot.say(embed=embed)
+            elif type == 2:
+                embed = discord.Embed(title="ITS A TRAP!", color=0x381010)
+                embed.set_image(url='https://media.giphy.com/media/8McNH1aXZnVyE/giphy.gif')
+                await self.bot.say(embed=embed)
+
+    @trap.group(pass_context=True)
+    async def add(self, ctx):
+        if ("https://" in ctx.message.content.lower() or "http://" in ctx.message.content.lower()):
+            url_pattern = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+            url = re.search(url_pattern, ctx.message.content.lower()).group(0)
+            self.trap_gifs.append(url)
+            dataIO.save_json('data/lewd/traps/traps.json', self.trap_gifs)
+        else:
+            try:
+                # normal submit.
+                comment = ctx.message.content[7:].lstrip(" ")
+                jsonstr = json.dumps(ctx.message.attachments[0])
+                jsondict = json.loads(jsonstr)
+                url = jsondict['url']
+                self.trap_gifs.append(url)
+                dataIO.save_json('data/lewd/traps/traps.json', self.trap_gifs)
+            except:
+                pass
 
 def setup(bot):
     bot.add_cog(Trap(bot))
