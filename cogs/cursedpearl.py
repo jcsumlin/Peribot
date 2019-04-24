5322ff76c371a7aa564b767d993a268361f62599
J_C___ <jcsumlin@gmail.com>
2019-04-24 00:10:27 -0500

    fixed levels and rank commands

--- cogs/cursedpearl.py
+++ cogs/cursedpearl.py
@@ -22,12 +22,12 @@ class CursedPearl:
     @commands.command(pass_context=True, no_pm=True)
     async def levels(self, ctx):
         if self.serverCheck(ctx):
-            await self.bot.send_file(ctx.message.channel, "data/card.png")
+            await self.bot.send_file(ctx.message.channel, "data/cp/card.png")
 
     @commands.command(pass_context=True, no_pm=True)
     async def rank(self, ctx):
         if self.serverCheck(ctx):
-            await self.bot.send_file(ctx.message.channel, "data/card.png")
+            await self.bot.send_file(ctx.message.channel, "data/cp/card.png")
 
     @commands.command(pass_context=True, no_pm=True)
     async def peridot(self, ctx):
