6979f7931e80e2d6985a4ae68c8b2207f624382e
Chat Sumlin <jcsumlin@gmail.com>
2019-08-10 15:59:50 -0400

    removed PIL cogs

--- cogs/fun.py
+++ cogs/fun.py
@@ -74,7 +74,7 @@ class Fun:
     async def on_message(self, message):
         if message.content.lower() == "f":
             await self.bot.add_reaction(message, u"\U0001F1EB")
-        if message.content.lower() == 'uwu' or message.content.lower() == 'owo':
+        if 'uwu' in message.content.lower() or 'owo' in message.content.lower():
             await self.bot.send_message(message.author, f"{message.content} again and see what happens! :mask:")
 
 
