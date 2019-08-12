9c5494288b2822a44659d0cec37b1446e9f6c052
Chat Sumlin <jcsumlin@gmail.com>
2019-08-11 21:32:11 -0400

    Converted to 1.3

--- cogs/bigmoji.py
+++ cogs/bigmoji.py
@@ -1,12 +1,9 @@
-import asyncio
-import functools
-import io
-import os
 import unicodedata
-from loguru import logger
 
 import aiohttp
+import discord
 from discord.ext import commands
+from loguru import logger
 
 try:
     import cairosvg
@@ -15,7 +12,7 @@ except:
     cairo = False
 
 
-class Bigmoji:
+class Bigmoji(commands.Cog):
 
     """Emoji tools"""
 
@@ -23,14 +20,11 @@ class Bigmoji:
         self.bot = bot
         self.session = aiohttp.ClientSession()
 
-    @commands.command(name="bigmoji", pass_context=True)
+    @commands.command(name="bigmoji")
     async def bigmoji(self, ctx, emoji):
         """Post a large .png of an emoji"""
-        channel = ctx.message.channel
         logger.debug(emoji)
         if emoji[0] == '<':
-            convert = False
-            name = emoji.split(':')[1]
             emoji_name = emoji.split(':')[2][:-1]
             anim = emoji.split(':')[0]
             if anim == '<a':
@@ -48,45 +42,12 @@ class Bigmoji:
                     # Sometimes occurs when the unicodedata library cannot
                     # resolve the name, however the image still exists
                     name.append("none")
-            name = '_'.join(name)
             if cairo:
-                convert = True
                 url = 'https://twemoji.maxcdn.com/2/svg/' + '-'.join(chars) + '.svg'
             else:
-                convert = False
                 url = 'https://twemoji.maxcdn.com/2/72x72/' + '-'.join(chars) + '.png'
-
-        async with self.session.get(url) as resp:
-            if resp.status != 200:
-                await self.bot.say('Emoji not found.')
-                return
-            img = await resp.read()
-
-        kwargs = {'parent_width': 1024,
-                  'parent_height': 1024}
-
-        task = functools.partial(Bigmoji.generate, img, convert, **kwargs)
-        task = self.bot.loop.run_in_executor(None, task)
-
-        try:
-            img = await asyncio.wait_for(task, timeout=15)
-        except asyncio.TimeoutError:
-            await self.bot.say("Image creation timed out.")
-            return
-
-        if emoji.split(':')[0] == '<a':
-            await self.bot.send_file(channel, img, filename=name + '.gif')
-        else:
-            await self.bot.send_file(channel, img, filename=name + '.png')
-
-    @staticmethod
-    def generate(img, convert, **kwargs):
-        # Designed to be run in executor to avoid blocking
-        if convert:
-            img = io.BytesIO(cairosvg.svg2png(bytestring=img, **kwargs))
-        else:
-            img = io.BytesIO(img)
-        return img
+        e = discord.Embed().set_image(url=url)
+        await ctx.send(embed=e)
 
     def __unload(self):
         self.session.close()
