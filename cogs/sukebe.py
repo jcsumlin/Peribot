df11fc12693e598fed92b92194b55555215125db
Chat Sumlin <jcsumlin@gmail.com>
2019-08-20 20:41:43 -0400

    updated to rewrite

--- cogs/sukebe.py
+++ cogs/sukebe.py
@@ -5,25 +5,27 @@ import discord
 from discord.ext import commands
 
 
-class Sukebe:
+class Sukebe(commands.Cog):
     """Paruru sensei can detect your Sukebe-ness."""
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True)
-    async def sukebe(self, ctx, user: discord.Member):
+    @commands.command()
+    async def sukebe(self, ctx, user: discord.Member = None):
         """Detects user's Sukebe-ness.
         157% accurate!"""
-
-        a = ctx.message.timestamp
+        if user is None:
+            user = ctx.author
+        a = ctx.message.created_at
         b = a.strftime("%Y-%m-%d %H:%M:%S.%f0")
         c = datetime.datetime.strptime(b[:7], '%Y-%m')
         d = c.timestamp()
 
-        random.seed(int(user.id) % int(d),)
+        random.seed(int(user.id) % int(d))
         x = ":fire:" * random.randint(1, 10)
-        await self.bot.say("{}'s Sukebe-ness is : ".format(user.name) + x)
+        print(x)
+        await ctx.send("{}'s Sukebe-ness is : ".format(user.display_name) + x)
 
 
 def setup(bot):
