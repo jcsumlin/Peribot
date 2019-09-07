305a544f44580fbc23f7b7658e9e61dbc0f407c6
Chat Sumlin <jcsumlin@gmail.com>
2019-09-07 00:03:26 -0400

    v1.3

--- cogs/sendasperibot.py
+++ cogs/sendasperibot.py
@@ -8,7 +8,7 @@ class SendAsPeribot(commands.Cog):
     @commands.command()
     async def send(self,ctx,  channel, *, message: str):
         if ctx.author.id == 204792579881959424:
-            channel2 = self.bot.get_channel(id=channel)
+            channel2 = self.bot.get_channel(int(channel))
             await channel2.send(message)
 
 def setup(bot):
