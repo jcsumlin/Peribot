dffb23ddd368d2ed816e509229b1f56c4df9b571
jcsumlin <jcsumlin@gmail.com>
2020-05-04 18:31:52 -0400

    added some api error checking

--- cogs/nookazon.py
+++ cogs/nookazon.py
@@ -20,9 +20,12 @@ class Nookazon(commands.Cog):
     async def nookazon(self, ctx, *, name):
         base_url = f"https://nookazon.com/api/items?variants=&search={urllib.parse.quote(name)}"
         async with self.session.get(base_url) as r:
+            if r.status != 200:
+                return ctx.send("API Error please try again later! :(")
             result = await r.json()
+
         if len(result["items"]) == 0:
-            await ctx.send(f"No items were found for {name}")
+            return await ctx.send(f"No items were found for {name}")
         first_item = result["items"][0]
         embed = discord.Embed(title="**Item**: {}".format(first_item["name"]), description="**Item Type**: {item_type} \n**Is DIY?**: {diy}\n**Buy Price**: {buy}\n**Sell Price**: {sell}\n**Average Price**: {avgprice}".format(
             item_type=first_item['type'],
