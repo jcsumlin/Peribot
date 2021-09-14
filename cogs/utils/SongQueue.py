9b38105c82beae95a7c4cefbc74f62b442d5a584
J_C___ <jcsumlin@pm.me>
2021-09-13 23:12:12 -0400

    Updated Music Cog
    Still based on https://gist.github.com/vbe0201/ade9b80f2d3b64643d854938d40a0a2d but its now heavily edited to fit the new Youtube_DL requirements.
    There are several features that are need to be implemented but at a base level this should work well.

---
+++ cogs/utils/SongQueue.py
@@ -0,0 +1,25 @@
+import asyncio
+import itertools
+import random
+
+class SongQueue(asyncio.Queue):
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            return list(itertools.islice(self._queue, item.start, item.stop, item.step))
+        else:
+            return self._queue[item]
+
+    def __iter__(self):
+        return self._queue.__iter__()
+
+    def __len__(self):
+        return self.qsize()
+
+    def clear(self):
+        self._queue.clear()
+
+    def shuffle(self):
+        random.shuffle(self._queue)
+
+    def remove(self, index: int):
+        del self._queue[index]
---
+++ cogs/utils/VoiceState.py
@@ -0,0 +1,76 @@
+import asyncio
+
+from async_timeout import timeout
+from discord.ext import commands, tasks
+
+from cogs.utils.SongQueue import SongQueue
+from cogs.utils.VoiceError import VoiceError
+
+
+class VoiceState:
+    def __init__(self, bot: commands.Bot, ctx: commands.Context):
+        self.bot = bot
+        self._ctx = ctx
+
+        self.current = None
+        self.voice = None
+        self.next = asyncio.Event()
+        self.songs = SongQueue()
+
+        self._loop = False
+
+        self.audio_player_task.start()
+
+    def __del__(self):
+        self.audio_player_task.cancel()
+
+    @property
+    def loop(self):
+        return self._loop
+
+    @loop.setter
+    def loop(self, value: bool):
+        self._loop = value
+
+    @property
+    def is_playing(self):
+        return self.voice and self.current
+
+    @tasks.loop()
+    async def audio_player_task(self):
+        while True:
+            self.next.clear()
+
+            if not self.loop:
+                # Try to get the next song within 3 minutes.
+                # If no song will be added to the queue in time,
+                # the player will disconnect due to performance
+                # reasons.
+                try:
+                    async with timeout(180):  # 3 minutes
+                        self.current = await self.songs.get()
+                except asyncio.TimeoutError:
+                    self.bot.loop.create_task(self.stop())
+                    return
+
+            self.voice.play(self.current.source, after=self.play_next_song)
+            await self.current.source.channel.send(embed=self.current.create_embed())
+
+            await self.next.wait()
+
+    def play_next_song(self, error=None):
+        if error:
+            raise VoiceError(str(error))
+
+        self.next.set()
+
+    def skip(self):
+        if self.is_playing:
+            self.voice.stop()
+
+    async def stop(self):
+        self.songs.clear()
+
+        if self.voice:
+            await self.voice.disconnect()
+            self.voice = None
